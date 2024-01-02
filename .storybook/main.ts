1ab4593cdbd55e18c35f5b69b572868a7368f616
Mi <thangkt@fpt.com>
2024-01-02 16:17:08 +0700

    Add build storbook script

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@nx/react/plugins/storybook',
+    'storybook-addon-designs',
   ],
   framework: {
     name: '@storybook/react-webpack5',
